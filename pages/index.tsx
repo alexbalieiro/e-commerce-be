fc120d2b537bd88d5243d991a4c06853398d3be9
Alex Bal <alexenri12@gmail.com>
2022-11-23 15:34:36 -0300

    add-env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  console.log(process.env.NEXT_PUBLIC_PRODUCTION_HOLA);
-  const nombre = process.env.NEXT_PUBLIC_PRODUCTION_HOLA;
+  console.log(process.env.NEXT_PUBLIC_HOLA);
+  const nombre = process.env.NEXT_PUBLIC_HOLA;
   return <h1>Hola {nombre}</h1>;
 };
 
